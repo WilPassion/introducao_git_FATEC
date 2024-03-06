9f633513f02d298840af884c68829a8568810092
William Santos <william.santos86@fatec.sp.gov.br>
2024-03-05 23:46:29 -0300

    apostila02(git-python): teste da funcao multiplicar

--- calculadora_teste.py
+++ calculadora_teste.py
@@ -5,8 +5,10 @@ def teste():
 
     soma = calculadora.somar(num1, num2)
     subtracao = calculadora.subtrair(num1, num2)
+    multiplicacao = calculadora.multiplicar(num1, num2)
 
     print("{} + {} = {}".format(num1, num2, soma))
     print("{} - {} = {}".format(num1, num2, subtracao))
+    print("{} x {} = {}".format(num1, num2, multiplicacao))
 
 teste()
