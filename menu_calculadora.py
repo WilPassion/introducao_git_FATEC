06a62e56ae998a5ebaef9b8c21a0226afc193609
William Santos <william.santos86@fatec.sp.gov.br>
2024-03-05 23:41:13 -0300

    funcao multiplicar implementada

--- menu_calculadora.py
+++ menu_calculadora.py
@@ -5,6 +5,7 @@ def menu():
         print("\nEscolha uma opção:")
         print("[1] Soma")
         print("[2] Subtrair")
+        print("[3] Multiplicar")
         print("[0] Sair\n")
 
         opcao = input("Digite a operação desejada: ")
@@ -17,7 +18,12 @@ def menu():
         if opcao == '2':
             num1 = float(input("Digite o primeiro número: "))
             num2 = float(input("Digite o segundo número: "))
-            print(num1,"x",num2, "= ", calculadora.subtrair(num1, num2))    
+            print(num1,"-",num2, "= ", calculadora.subtrair(num1, num2))    
+
+        if opcao == '3':
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            print(num1,"x",num2, "= ", calculadora.multiplicar(num1, num2))       
         
         elif opcao == '0':
             print("Encerrando...")
