8cc35f2db49f446ef45c60e3685f42c985185ea7
William Santos <william.santos86@fatec.sp.gov.br>
2024-03-05 23:29:57 -0300

    apostila02(git-python): teste da funcao subtrair

--- calculadora_teste.py
+++ calculadora_teste.py
@@ -4,7 +4,9 @@ def teste():
     num2 = 2
 
     soma = calculadora.somar(num1, num2)
+    subtracao = calculadora.subtrair(num1, num2)
 
     print("{} + {} = {}".format(num1, num2, soma))
+    print("{} - {} = {}".format(num1, num2, subtracao))
 
 teste()
