a7e65d88f80309bbf99675bc3a70c1e7d2f1926b
William Santos <william.santos86@fatec.sp.gov.br>
2024-03-05 23:54:09 -0300

    apostila02(git-python): teste da funcao dividir

--- calculadora_teste.py
+++ calculadora_teste.py
@@ -1,14 +1,16 @@
 import calculadora
 def teste():
-    num1 = 1
-    num2 = 2
+    num1 = 10
+    num2 = 5
 
     soma = calculadora.somar(num1, num2)
     subtracao = calculadora.subtrair(num1, num2)
     multiplicacao = calculadora.multiplicar(num1, num2)
+    divisao = calculadora.dividir(num1, num2)
 
     print("{} + {} = {}".format(num1, num2, soma))
     print("{} - {} = {}".format(num1, num2, subtracao))
     print("{} x {} = {}".format(num1, num2, multiplicacao))
+    print("{} / {} = {}".format(num1, num2, divisao))
 
 teste()
